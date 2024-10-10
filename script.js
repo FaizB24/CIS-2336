1750ffca31d18c0b821448d1df2ff5eff3440b97
FaizB24 <fbadmus@cougarnet.uh.edu>
2024-10-10 17:23:53 -0500

    exam submission

---
+++ script.js
@@ -0,0 +1,18 @@
+//do not change
+let students_cis2337 = ["Peter", "Paris", "Ben", "Soledad"];
+let students_cis2348 = ["John", "Maria", "Trevor"];
+
+function alert_function() {
+    // Print each student's name in CIS 2337 to the console
+    console.log("CIS 2337 Students:");
+    for (let i = 0; i < studentsCIS2337.length; i++) {
+        console.log(studentsCIS2337[i]);
+    }
+
+    // Calculate the total number of students
+    let totalStudents = studentsCIS2337.length;
+
+    // Display an alert with the total number of students
+    alert("The total number of students is: " + totalStudents);
+}s
+    
